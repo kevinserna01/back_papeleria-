a1f7b72799a57e160d745202b3cc4b9e39c621b1
kevin <kevinfernandoserna11@gmail.com>
2025-04-12 16:55:02 -0500

    vercel cors

--- index.js
+++ index.js
@@ -4,32 +4,30 @@ const router = require('./routes/papeleria.routes.js');
 const cors = require('cors');
 require('dotenv').config();
 
-
 const app = express();
 
 const corsOptions = {
     origin: [
       'http://localhost:5173', // Para desarrollo local
       'https://back-papeleria-two.vercel.app', // Para producción
-      // Agrega otros orígenes permitidos si es necesario
     ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos HTTP permitidos
-    allowedHeaders: ['Content-Type', 'Authorization'], // Headers permitidos
-    credentials: true, // Permite credenciales
-    optionsSuccessStatus: 200 // Para navegadores antiguos que no soportan 204
-  };
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    optionsSuccessStatus: 200
+};
+
+// Aplicar CORS con las opciones configuradas
+app.use(cors(corsOptions));
 
 // Middleware para analizar datos codificados y JSON
 app.use(urlencoded({ extended: true }));
 app.use(json());
-app.use(cors());
 
 // Manejador para la ruta raíz
 app.get('/', (req, res) => {
     res.send('Bienvenido al backend de Kevin!');
 });
-
-
 
 app.use('/v1/papeleria', router);
 
